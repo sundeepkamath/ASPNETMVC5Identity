e957049471068722979e9234f2d127e90e4b8905
Sundeep Kamath <sundeep.g.kamath@gmail.com>
2015-09-05 15:59:32 +0530

    Added LoginModel and logic to accept login data from user, authenticate that data and login user on success.

--- ASPNetMVC5Identity/Controllers/AuthController.cs
+++ ASPNetMVC5Identity/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ASPNetMVC5Identity.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,57 @@ namespace ASPNetMVC5Identity.Controllers
 {
     public class AuthController : Controller
     {
-        // GET: Auth
-        public ActionResult Login()
+        [HttpGet]
+        public ActionResult Login(string returnUrl)
         {
+            var model = new LoginModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // The user related information have been hardcoded for the time being.
+            // In production, the hard coded values would be fetched from the database using the new ASP.Net Identity UserManager.
+            if (model.Email == "admin@admin.com" && model.Password == "password")
+            {
+                var identity = new ClaimsIdentity(
+                    new[] {
+                            new Claim(ClaimTypes.Name, "Sundeep"),
+                            new Claim(ClaimTypes.Email, "sk@test.com"),
+                            new Claim(ClaimTypes.Country, "India")
+                          },
+                    "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+
+                authManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            // In case user authentication fails.
+            ModelState.AddModelError("", "Invalid email or password");
             return View();
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
         }
     }
 }
