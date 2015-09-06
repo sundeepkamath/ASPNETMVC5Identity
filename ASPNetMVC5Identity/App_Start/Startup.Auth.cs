20090ea6154aa2fd68d5a6f675c4b2e124d2f3b9
Sundeep Kamath <sundeep.g.kamath@gmail.com>
2015-09-06 09:59:49 +0530

    Refactored the Startup class to separate authentication logic.

---
+++ ASPNetMVC5Identity/App_Start/Startup.Auth.cs
@@ -0,0 +1,19 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+
+namespace ASPNetMVC5Identity
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/auth/login")
+            });
+        }
+    }
+}
--- ASPNetMVC5Identity/Startup.cs
+++ ASPNetMVC5Identity/Startup.cs
@@ -1,22 +1,15 @@
-﻿using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
-using Owin;
-using Microsoft.AspNet.Identity;
+﻿using Owin;
 
 namespace ASPNetMVC5Identity
 {
     //To initialize the OWIN identity components we need to add a Startup class to the project.
     //This class should have a method Configuration that takes an OWIN IAppBuilder instance as a parameter. 
-    public class Startup
+    public partial class Startup
     {
         //This class will be automatically located and initialized by the OWIN host
         public void Configuration(IAppBuilder app)
         {
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType =  DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/auth/login")
-            });
+            ConfigureAuth(app);
         }
     }
 }
